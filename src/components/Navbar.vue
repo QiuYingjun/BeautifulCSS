d3161632eb9442316350ceed9a5fd72748ffbdbe
Yingjun Qiu <qyj9999@gmail.com>
2021-08-30 00:17:43 +0900

    首页轮播

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,25 +1,37 @@
 <template>
-  <a-menu v-model:selectedKeys="Home" mode="horizontal">
+  <a-menu v-model:selectedKeys="current" mode="horizontal">
     <a-menu-item key="Home">
       <HomeOutlined />
-      <router-link to="/">
-        Home
-      </router-link>
+      <router-link to="/"> Home </router-link>
     </a-menu-item>
     <a-menu-item key="EditPanel">
       <ExperimentOutlined />
-      <router-link to="/EditPanel">
-        Panel
-      </router-link>
+      <router-link to="/EditPanel"> Panel </router-link>
     </a-menu-item>
   </a-menu>
 </template>
 <script lang="ts">
 import { ExperimentOutlined, HomeOutlined } from "@ant-design/icons-vue";
+
 export default {
+  data() {
+    return {
+      current: ["Home"],
+    };
+  },
   components: {
     ExperimentOutlined,
     HomeOutlined,
+  },
+  watch: {
+    $route(to, from) {
+      console.log(to.fullPath, from.fullPath);
+      if (to.fullPath == "/") {
+        this.current[0] = "Home";
+      } else if (to.fullPath.indexOf("EditPanel") > -1) {
+        this.current[0] = "EditPanel";
+      }
+    },
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,42 @@
 <!-- home -->
 <template>
   <div class="home">
-    home
+    <a-carousel autoplay>
+      <div class="one" v-for="p in projects" :key="p.name">
+        <router-link :to="'/EditPanel/' + p.name">
+          <h3>{{ p.name }}</h3>
+          <img :src="p.image_url" />
+        </router-link>
+      </div>
+    </a-carousel>
   </div>
 </template>
-
+<style scoped>
+div.one {
+  height: 800px;
+  text-align: center;
+}
+div.one img {
+  height: 100%;
+  display: inline-block;
+}
+</style>
 <script>
 export default {
   name: "Home",
+  mounted() {
+    if (this.$store.state.projects.length == 0) {
+      this.$store.dispatch("getProjectListFromAPI");
+    }
+  },
+  computed: {
+    projects() {
+      if (this.$store.state.projects.length < 5) {
+        return this.$store.state.projects;
+      } else {
+        return this.$store.state.projects.slice(0, 4);
+      }
+    },
+  },
 };
 </script>
